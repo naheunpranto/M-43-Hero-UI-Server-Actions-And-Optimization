732e19612c8fd9da4a8f808c5458db60d6eebb97
Naheun Alam <naheunpranto258@gmail.com>
2026-06-20 17:00:22 +0600

    Navigation bar and set theme on the website

---
+++ src/components/ThemeToggle.jsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useTheme } from "next-themes";
+
+const ThemeToggle = () => {
+  const { theme, setTheme } = useTheme();
+  return (
+    <div>
+        <button onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+        Toggle {theme === "dark" ? "Light" : "Dark"} Mode
+        </button>
+    </div>
+  );
+};
+
+export default ThemeToggle;

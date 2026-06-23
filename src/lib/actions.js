e39356ca3d8d2cf12284c32a82425bfb526b967b
Naheun Alam <naheunpranto258@gmail.com>
2026-06-23 21:11:21 +0600

    Revalidation after server action to display form data

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,6 +1,23 @@
+import { revalidatePath } from "next/cache";
+import { postTask } from "./tasks";
+
 export const createATask = async(formData) => {
     'use server'
-    const name = formData.get('name');
+    // const title = formData.get('title');
+    // const description = formData.get('description');
+    // const priority = formData.get('priority');
+    // const status = formData.get('status');
+    // const assignedTo = formData.get('assignedTo');
 
-    console.log('Adding a task with name:', formData);
+    // const newTask = {title, description, priority, status, assignedTo};
+
+    const newTask = Object.fromEntries(formData.entries());
+
+    console.log('Adding a task with name:', newTask);
+
+    const res = await postTask(newTask);
+
+    if(res.ok){
+        revalidatePath('/tasks')
+    }
 }
